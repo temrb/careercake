8a1c740dade885d4b29c7e56c9ad1e6b2958334a
tem <temurbakhriddinov@gmail.com>
2022-06-22 23:43:22 -0500

    clean up refactor

--- src/components/home/track/track-board/category.component.tsx
+++ src/components/home/track/track-board/category.component.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
-import { useAppDispatch, useAppSelector } from '../../../../hooks/useRedux';
+import { useAppDispatch } from '../../../../hooks/useRedux';
 import { setModal } from '../../../../redux/features/utilsSlice';
 import { setCategoryInput } from '../../../../redux/features/trackSlice';
-
-interface RootState {
-  utils: any;
-}
-
+import { OfficeBuildingIcon, BriefcaseIcon } from '@heroicons/react/solid';
 interface Props {
   title: string;
   backgroundColor: string;
@@ -15,25 +11,24 @@ interface Props {
   contentHeadingColor: string;
   setCategoryOpen: any;
   categoryOpen: boolean;
-  contentCount: any;
-  children: React.ReactNode;
+  cards: any;
+  cardColor: string;
 }
 
 const Category = (Props: Props) => {
-  const modal = useAppSelector((state: RootState) => state.utils.modal);
-
   const handleOpenModal = () => {
     useAppDispatch(setModal(true));
     useAppDispatch(setCategoryInput(Props.title));
   };
-
   return (
     <div>
       <button
         className={`flex justify-between font-semibold py-3 ${Props.backgroundColor} shadow-md px-3 items-center rounded-lg w-full`}
         onClick={() => Props.setCategoryOpen(!Props.categoryOpen)}
       >
-        <span className='flex justify-start items-center'>{Props.title}</span>
+        <span className='flex justify-start text-white items-center'>
+          {Props.title}
+        </span>
         <span className='flex justify-end items-center gap-2'>
           <span className='h-7 w-7 bg-white shadow-md rounded-full cursor-pointer'>
             {Props.categoryOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
@@ -46,21 +41,55 @@ const Category = (Props: Props) => {
         >
           <div className='grid grid-cols-2 items-center'>
             <div
-              className={`font-semibold flex justify-start text-xs mx-auto ${Props.contentHeadingColor} p-3 rounded-lg border-b-2 ${Props.contentBorderColor}`}
+              className={`font-semibold flex justify-start text-xs mx-auto ${Props.contentHeadingColor} p-3 rounded-lg border-b-2  ${Props.contentBorderColor}`}
             >
-              {Props.contentCount.length} items
+              <div className='truncate overflow-hidden ...'>
+                {
+                  Props.cards.filter(
+                    (card: { category: string }) =>
+                      card.category === Props.title
+                  ).length
+                }{' '}
+                items
+              </div>
             </div>
-            {!modal && (
-              <button
-                type='submit'
-                className={`relative inline-flex flex-initial items-center justify-center rounded-3xl ${Props.backgroundColor} px-4 py-2 font-bold leading-6 text-white shadow-md`}
-                onClick={() => handleOpenModal()}
-              >
-                Add
-              </button>
-            )}
+            <button
+              type='submit'
+              className={`relative truncate inline-flex flex-initial items-center justify-center rounded-3xl ${Props.backgroundColor} px-4 py-2 font-bold leading-6 text-white shadow-md`}
+              onClick={() => handleOpenModal()}
+            >
+              Add
+            </button>
           </div>
-          {Props.children}
+          {Props.cards
+            .filter(
+              (card: { category: string }) => card.category === Props.title
+            )
+            .map((category: any) => (
+              <div
+                key={category.id}
+                className={`${Props.cardColor} p-2 rounded-md bg-white`}
+              >
+                <div className='grid space-y-1 capitalize'>
+                  <div className='flex items-center gap-2 truncate '>
+                    <div className='bg-white p-1 rounded-lg'>
+                      <BriefcaseIcon className='h-4' />
+                    </div>
+                    <h1 className='flex font-semibold '>{category.job}</h1>
+                  </div>
+                  <div className='flex truncate'>
+                    <div className='flex items-center gap-2'>
+                      <div className='bg-white p-1 rounded-lg'>
+                        <OfficeBuildingIcon className='h-4' />
+                      </div>
+                      <h2 className='flex text-ellipsis text-sm'>
+                        {category.company}
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       )}
     </div>
--- src/components/home/track/track-board/track-board.component.tsx
+++ src/components/home/track/track-board/track-board.component.tsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { OfficeBuildingIcon, BriefcaseIcon } from '@heroicons/react/solid';
 import Modal from '@mui/material/Modal';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/useRedux';
 import { setModal } from '../../../../redux/features/utilsSlice';
-import { setCategoryInput } from '../../../../redux/features/trackSlice';
 import BoardModal from './board-modal.component';
 import Category from './category.component';
 import { API } from 'aws-amplify';
 import { listCards } from '../../../../graphql/queries';
 import { Card, ListCardsQuery } from '../../../../API';
 import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 interface RootState {
   utils: any;
@@ -44,8 +43,23 @@ const TrackBoard = () => {
     };
     fetchCardsFromApi();
   }, []);
-
   console.log('cards', cards);
+
+  function reduce(arg0: (acc: Card[], card: Card) => Card[], cards: Card[]) {
+    throw new Error('Function not implemented.');
+  }
+
+  const onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+    const newCards = reduce((acc: Card[], card: Card) => {
+      if (card.id === result.draggableId) {
+        acc.push(card);
+      }
+      return acc;
+    }, cards);
+  };
 
   return (
     <main>
@@ -53,204 +67,60 @@ const TrackBoard = () => {
       <Modal open={modal} onClose={handleCloseModal}>
         <>{<BoardModal />}</>
       </Modal>
-
       {/* category board */}
       <div className='pt-6 px-6 grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-4 overflow-auto'>
-        <Category
-          title='Wishlist'
-          backgroundColor='bg-gray-400/80'
-          contentBorderColor='border-gray-400/50'
-          contentHeadingColor='text-gray-500'
-          setCategoryOpen={setWishlistOpen}
-          categoryOpen={wishlistOpen}
-          contentCount={cards.filter((card) => card.category === 'Wishlist')}
-        >
-          {cards
-            .filter((card) => card.category === 'Wishlist')
-            .map((category) => (
-              <div
-                key={category.id}
-                className='bg-gray-400/50 p-2 rounded-md bg-white'
-              >
-                <div className='grid space-y-1'>
-                  <div className='flex items-center gap-2'>
-                    <div className='bg-white p-1 rounded-lg'>
-                      <BriefcaseIcon className='h-4' />
-                    </div>
-                    <h1 className='flex font-semibold text-ellipsis'>
-                      {category.job}
-                    </h1>
-                  </div>
-                  <div className='flex '>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-white p-1 rounded-lg'>
-                        <OfficeBuildingIcon className='h-4' />
-                      </div>
-                      <h2 className='flex text-ellipsis text-sm'>
-                        {category.company}
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Category>
-        <Category
-          title='Applied'
-          backgroundColor='bg-orange-500/80'
-          contentBorderColor='border-orange-400/50'
-          contentHeadingColor='text-orange-500'
-          setCategoryOpen={setAppliedOpen}
-          categoryOpen={appliedOpen}
-          contentCount={cards.filter((card) => card.category === 'Applied')}
-        >
-          {cards
-            .filter((card) => card.category === 'Applied')
-            .map((category) => (
-              <div
-                key={category.id}
-                className='bg-orange-400/50 p-2 rounded-md bg-white'
-              >
-                <div className='grid space-y-1'>
-                  <div className='flex items-center gap-2'>
-                    <div className='bg-white p-1 rounded-lg'>
-                      <BriefcaseIcon className='h-4' />
-                    </div>
-                    <h1 className='flex font-semibold text-ellipsis'>
-                      {category.job}
-                    </h1>
-                  </div>
-                  <div className='flex '>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-white p-1 rounded-lg'>
-                        <OfficeBuildingIcon className='h-4' />
-                      </div>
-                      <h2 className='flex text-ellipsis text-sm'>
-                        {category.company}
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Category>
-        <Category
-          title='Interview'
-          backgroundColor='bg-blue-500/80'
-          contentBorderColor='border-blue-400/50'
-          contentHeadingColor='text-blue-500'
-          setCategoryOpen={setInterviewOpen}
-          categoryOpen={interviewOpen}
-          contentCount={cards.filter((card) => card.category === 'Interview')}
-        >
-          {cards
-            .filter((card) => card.category === 'Interview')
-            .map((category) => (
-              <div
-                key={category.id}
-                className='bg-blue-400/50 p-2 rounded-md bg-white'
-              >
-                <div className='grid space-y-1'>
-                  <div className='flex items-center gap-2'>
-                    <div className='bg-white p-1 rounded-lg'>
-                      <BriefcaseIcon className='h-4' />
-                    </div>
-                    <h1 className='flex font-semibold text-ellipsis'>
-                      {category.job}
-                    </h1>
-                  </div>
-                  <div className='flex '>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-white p-1 rounded-lg'>
-                        <OfficeBuildingIcon className='h-4' />
-                      </div>
-                      <h2 className='flex text-ellipsis text-sm'>
-                        {category.company}
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Category>
-        <Category
-          title='Offer'
-          backgroundColor='bg-green-500/80'
-          contentBorderColor='border-green-400/50'
-          contentHeadingColor='text-green-500'
-          setCategoryOpen={setOfferOpen}
-          categoryOpen={offerOpen}
-          contentCount={cards.filter((card) => card.category === 'Offer')}
-        >
-          {cards
-            .filter((card) => card.category === 'Offer')
-            .map((category) => (
-              <div
-                key={category.id}
-                className='bg-green-400/50 p-2 rounded-md bg-white'
-              >
-                <div className='grid space-y-1'>
-                  <div className='flex items-center gap-2'>
-                    <div className='bg-white p-1 rounded-lg'>
-                      <BriefcaseIcon className='h-4' />
-                    </div>
-                    <h1 className='flex font-semibold text-ellipsis'>
-                      {category.job}
-                    </h1>
-                  </div>
-                  <div className='flex '>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-white p-1 rounded-lg'>
-                        <OfficeBuildingIcon className='h-4' />
-                      </div>
-                      <h2 className='flex text-ellipsis text-sm'>
-                        {category.company}
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Category>
-        <Category
-          title='Rejected'
-          backgroundColor='bg-red-500/80'
-          contentBorderColor='border-red-400/50'
-          contentHeadingColor='text-red-500'
-          setCategoryOpen={setRejectedOpen}
-          categoryOpen={rejectedOpen}
-          contentCount={cards.filter((card) => card.category === 'Rejected')}
-        >
-          {cards
-            .filter((card) => card.category === 'Rejected')
-            .map((category) => (
-              <div
-                key={category.id}
-                className='bg-red-400/50 p-2 rounded-md bg-white'
-              >
-                <div className='grid space-y-1'>
-                  <div className='flex items-center gap-2'>
-                    <div className='bg-white p-1 rounded-lg'>
-                      <BriefcaseIcon className='h-4' />
-                    </div>
-                    <h1 className='flex font-semibold text-ellipsis'>
-                      {category.job}
-                    </h1>
-                  </div>
-                  <div className='flex '>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-white p-1 rounded-lg'>
-                        <OfficeBuildingIcon className='h-4' />
-                      </div>
-                      <h2 className='flex text-ellipsis text-sm'>
-                        {category.company}
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Category>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Category
+            title='Wishlist'
+            backgroundColor='bg-gray-400'
+            contentBorderColor='border-gray-400/50'
+            contentHeadingColor='text-gray-500'
+            setCategoryOpen={setWishlistOpen}
+            categoryOpen={wishlistOpen}
+            cards={cards.filter((card) => card.category === 'Wishlist')}
+            cardColor='bg-gray-400/50'
+          />
+          <Category
+            title='Applied'
+            backgroundColor='bg-orange-500/80'
+            contentBorderColor='border-orange-400/50'
+            contentHeadingColor='text-orange-500'
+            setCategoryOpen={setAppliedOpen}
+            categoryOpen={appliedOpen}
+            cards={cards.filter((card) => card.category === 'Applied')}
+            cardColor='bg-orange-400/50'
+          />
+          <Category
+            title='Interview'
+            backgroundColor='bg-blue-500/80'
+            contentBorderColor='border-blue-400/50'
+            contentHeadingColor='text-blue-500'
+            setCategoryOpen={setInterviewOpen}
+            categoryOpen={interviewOpen}
+            cards={cards.filter((card) => card.category === 'Applied')}
+            cardColor='bg-blue-400/50'
+          />
+          <Category
+            title='Offer'
+            backgroundColor='bg-green-500/80'
+            contentBorderColor='border-green-400/50'
+            contentHeadingColor='text-green-500'
+            setCategoryOpen={setOfferOpen}
+            categoryOpen={offerOpen}
+            cards={cards.filter((card) => card.category === 'Offer')}
+            cardColor='bg-green-400/50'
+          />
+          <Category
+            title='Rejected'
+            backgroundColor='bg-red-500/80'
+            contentBorderColor='border-red-400/50'
+            contentHeadingColor='text-red-500'
+            setCategoryOpen={setRejectedOpen}
+            categoryOpen={rejectedOpen}
+            cards={cards.filter((card) => card.category === 'Rejected')}
+            cardColor='bg-red-400/50'
+          />
+        </DragDropContext>
       </div>
     </main>
   );
